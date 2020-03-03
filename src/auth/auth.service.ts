9a19e304a855037c2675c47b818e7efaccba386d
seyha <seyha.hiem@sisgroup.sg>
2020-03-03 15:03:31 +0700

    add swagger

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { AuthCredentailDto } from './dto/auth-credential.dto';
 import { User } from './user.interface';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -11,6 +12,8 @@ export class AuthService {
     constructor(@InjectModel('User') private userModel: Model<User>, private jwtService: JwtService) { }
 
     async signup(authCredentailDto: AuthCredentailDto): Promise<User> {
+        const hashPassword = await bcrypt.hash(authCredentailDto.password, 12);
+        authCredentailDto.password = hashPassword;
         const createdUser = new this.userModel(authCredentailDto);
         return createdUser.save();
     }
